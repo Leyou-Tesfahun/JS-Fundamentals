cd4d59a591b2f6d25ac6d6e8574c7f5eafa76db0
Leyou-Tesfahun <leyoutesfahun09@gmail.com>
2025-07-28 06:04:03 -0700

    Create 7-multi_c.js

---
+++ 7-multi_c.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// Script that prints "C is fun" x times
+
+const x = parseInt(process.argv[2]);
+
+if (isNaN(x)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
+}
